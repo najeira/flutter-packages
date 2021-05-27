abeea11ebca348c94b944c04893d4388a0b557cd
najeira <najeira@gmail.com>
2021-05-27 19:01:50 +0900

    refactor: DropdownDatePicker

--- packages/dropdown_datepicker/lib/src/dropdown_date_picker.dart
+++ packages/dropdown_datepicker/lib/src/dropdown_date_picker.dart
@@ -1,38 +1,56 @@
 import 'package:flutter/material.dart';
 
-import '../src/date.dart';
-import '../src/date_format.dart';
-import '../src/date_hint.dart';
-import '../src/date_util.dart';
-import '../src/nullable_valid_date.dart';
-import '../src/valid_date.dart';
+import 'date.dart';
+import 'date_format.dart';
+import 'date_hint.dart';
+import 'date_util.dart';
+import 'nullable_valid_date.dart';
 
 /// Creates a [DropdownDatePicker] widget instance
 ///
 /// Displays year, month and day [DropdownButton] widgets in a [Row]
-// ignore: must_be_immutable
 class DropdownDatePicker extends StatefulWidget {
-  /// Stores the date format how [DropdownButton] widgets should be build.
+  /// Creates an instance of [DropdownDatePicker].
+  ///
+  /// [firstYear] must be before [lastYear]
+  /// and [initialDate] must be between their range
+  ///
+  /// [initialDate] is optional, if not provided a hintText
+  /// will be shown in their [DropDownButton]'s
+  ///
+  /// By default [dateFormat] is [DateFormat.ymd].
+  const DropdownDatePicker({
+    Key? key,
+    this.firstYear = 1900,
+    this.lastYear = 2999,
+    this.initialDate = const NullableValidDate.nullDate(),
+    this.dateFormat = DateFormat.ymd,
+    this.dateHint = const DateHint(),
+    this.textStyle,
+    this.underline = const _Underline(),
+    this.ascending = true,
+  })  : assert(firstYear < lastYear, 'First year must be before last year.'),
+        super(key: key);
+
+  /// The date format how [DropdownButton] widgets should be build.
   final DateFormat dateFormat;
 
-  /// Mimimum date value that [DropdownButton] widgets can have.
-  final ValidDate firstDate;
+  /// Initial date value.
+  final Date initialDate;
 
-  /// Maximum date value that [DropdownButton] widgets can have.
-  final ValidDate lastDate;
+  /// Mimimum year value that [DropdownButton] widgets can have.
+  final int firstYear;
+
+  /// Maximum year value that [DropdownButton] widgets can have.
+  final int lastYear;
 
   /// Text style of the dropdown button and it's menu items
   final TextStyle? textStyle;
 
   /// The widget to use for drawing the drop-down button's underline.
   ///
-  /// Defaults to a 2.0 height bottom container with Theme.of(context).primaryColor
-  final Widget? underLine;
-
-  /// The background color of the dropdown.
-  ///
-  /// If it is not provided, the theme's [ThemeData.canvasColor] will be used instead.
-  final Color? dropdownColor;
+  /// Defaults to a 1.0 height bottom container with Theme.of(context).dividerColor
+  final Widget? underline;
 
   /// If true [DropdownMenuItem]s will be built in ascending order
   final bool ascending;
@@ -42,262 +60,202 @@ class DropdownDatePicker extends StatefulWidget {
   /// Default is: 'yyyy', 'mm', 'dd'
   final DateHint dateHint;
 
-  Date _currentDate;
-
-  /// Holds the currently selected date
-  Date get currentDate => _currentDate;
-
-  /// Returns currently selected day
-  int? get day {
-    return _currentDate.day;
-  }
-
-  /// Returns currently selected month
-  int? get month {
-    return _currentDate.month;
-  }
-
-  /// Returns currently selected year
-  int? get year {
-    return _currentDate.year;
-  }
-
-  /// Returns date as String by a [separator]
-  /// based on [dateFormat]
-  String? getDate([String separator = '-']) {
-    String? date;
-    var year = _currentDate.year;
-    var month = Date.toStringWithLeadingZeroIfLengthIsOne(_currentDate.month);
-    var day = Date.toStringWithLeadingZeroIfLengthIsOne(_currentDate.day);
-
-    switch (dateFormat) {
-      case DateFormat.ymd:
-        date = '$year$separator$month$separator$day';
-        break;
-      case DateFormat.dmy:
-        date = '$day$separator$month$separator$year';
-        break;
-      case DateFormat.mdy:
-        date = '$month$separator$day$separator$year';
-        break;
-    }
-    return date;
-  }
-
-  /// Creates an instance of [DropdownDatePicker].
-  ///
-  /// [firstDate] must be before [lastDate]
-  /// and [initialDate] must be between their range
-  ///
-  /// [initialDate] is optional, if not provided a hintText
-  /// will be shown in their [DropDownButton]'s
-  ///
-  /// By default [dateFormat] is [DateFormat.ymd].
-  DropdownDatePicker({
-    required this.firstDate,
-    required this.lastDate,
-    Date initialDate = const NullableValidDate.nullDate(),
-    this.dateFormat = DateFormat.ymd,
-    this.dateHint = const DateHint(),
-    this.dropdownColor,
-    this.textStyle,
-    this.underLine,
-    this.ascending = true,
-    final Key? key,
-  })  : assert(firstDate < lastDate, 'First date must be before last date.'),
-        assert(_isValidInitialDate(initialDate, firstDate, lastDate),
-            'Initial date must be null or between first and last date.'),
-        _currentDate = initialDate,
-        super(key: key);
-
-  static bool _isValidInitialDate(Date date, Date firstDate, Date lastDate) {
-    if (date.year == null) return true;
-    if (date.year! < firstDate.year!) return false;
-    if (date.year! > lastDate.year!) return false;
-    if (date.month == null) return true;
-    if (date.month! < firstDate.month!) return false;
-    if (date.month! > lastDate.month!) return false;
-    if (date.day == null) return true;
-    if (date.day! < firstDate.day!) return false;
-    if (date.day! > lastDate.day!) return false;
-
-    return true;
-  }
-
   @override
   _DropdownDatePickerState createState() => _DropdownDatePickerState();
 }
 
 class _DropdownDatePickerState extends State<DropdownDatePicker> {
-  List<DropdownMenuItem<int>> _buildDropdownMenuItemList(int? min, int? max) {
-    return _intGenerator(min, max, widget.ascending)
-        .map(
-          (i) => DropdownMenuItem<int>(
-            value: i,
-            child: Container(
-              alignment: Alignment.center,
-              child: Text(
-                Date.toStringWithLeadingZeroIfLengthIsOne(i),
-                textAlign: TextAlign.center,
-                style: widget.textStyle,
-              ),
-            ),
-          ),
-        )
-        .toList();
+  Date? _currentDate;
+
+  /// Holds the currently selected date
+  Date get currentDate => _currentDate ?? widget.initialDate;
+
+  /// Returns currently selected day
+  int? get day {
+    return currentDate.day;
   }
 
-  Widget _buildDropdownButton({
-    required final int? initialValue,
-    required final String hint,
-    required final Function onChanged,
-    required final List<DropdownMenuItem<int>> items,
-  }) {
-    return DropdownButton<int>(
-      dropdownColor: widget.dropdownColor,
-      underline: widget.underLine ??
-          Container(
-            color: Theme.of(context).primaryColor,
-            height: 2,
-          ),
-      value: initialValue,
-      hint: Text(
-        hint, // dd, mm, yyyy
-        style: widget.textStyle,
-      ),
-      onChanged: (val) => Function.apply(onChanged, [val]),
-      items: items,
-    );
+  /// Returns currently selected month
+  int? get month {
+    return currentDate.month;
   }
 
-  Iterable<int> _intGenerator(int? start, int? end, bool ascending) sync* {
-    if (ascending) {
-      for (var i = start!; i <= end!; i++) {
-        yield i;
-      }
-    } else {
-      for (var i = end!; i >= start!; i--) {
-        yield i;
-      }
-    }
+  /// Returns currently selected year
+  int? get year {
+    return currentDate.year;
   }
 
-  Widget _buildYearDropdownButton() {
-    return _buildDropdownButton(
-      items: _buildDropdownMenuItemList(
-        widget.firstDate.year,
-        widget.lastDate.year,
-      ),
-      initialValue: widget._currentDate.year,
-      hint: widget.dateHint.year,
-      onChanged: (final year) => setState(
-          () => widget._currentDate = widget._currentDate.copyWith(year: year)),
-    );
-  }
-
-  Widget _buildMonthDropdownButton() {
-    int? minMonth = 1;
-    int? maxMonth = 12;
-
-    if (widget._currentDate.year != null) {
-      if (widget.firstDate.year == widget._currentDate.year) {
-        minMonth = widget.firstDate.month;
-        if (widget._currentDate.month != null &&
-            widget._currentDate.month! < widget.firstDate.month!) {
-          widget._currentDate = widget._currentDate.copyWith(month: minMonth);
-        }
-      }
-      if (widget.lastDate.year == widget._currentDate.year) {
-        maxMonth = widget.lastDate.month;
-        if (widget._currentDate.month != null &&
-            widget._currentDate.month! > widget.lastDate.month!) {
-          widget._currentDate = widget._currentDate.copyWith(month: maxMonth);
-        }
-      }
-    } else {
-      widget._currentDate =
-          widget._currentDate.copyWith(month: widget._currentDate.month);
-    }
-    return _buildDropdownButton(
-      items: _buildDropdownMenuItemList(
-        minMonth,
-        maxMonth,
-      ),
-      initialValue: widget._currentDate.month,
-      hint: widget.dateHint.month,
-      onChanged: (final month) => setState(() =>
-          widget._currentDate = widget._currentDate.copyWith(month: month)),
-    );
-  }
-
-  Widget _buildDayDropdownButton() {
-    int? minDay = 1;
-    int? maxDay = DateUtil.daysInDate(
-      month: widget._currentDate.month,
-      year: widget._currentDate.year,
-    );
-
-    if (widget._currentDate.month != null) {
-      if (widget._currentDate.year != null) {
-        if (widget.firstDate.year == widget._currentDate.year &&
-            widget.firstDate.month! >= widget._currentDate.month!) {
-          minDay = widget.firstDate.day;
-          if (widget._currentDate.day != null &&
-              widget.firstDate.day! >= widget._currentDate.day!) {
-            widget._currentDate = widget._currentDate.copyWith(day: minDay);
-          }
-        }
-        if (widget.lastDate.year == widget._currentDate.year &&
-            widget.lastDate.month! <= widget._currentDate.month!) {
-          maxDay = widget.lastDate.day;
-          if (widget._currentDate.day != null &&
-              widget.lastDate.day! <= widget._currentDate.day!) {
-            widget._currentDate = widget._currentDate.copyWith(day: maxDay);
-          }
-        }
-      }
-    }
-
-    return _buildDropdownButton(
-      items: _buildDropdownMenuItemList(minDay, maxDay),
-      initialValue: widget._currentDate.day,
-      hint: widget.dateHint.day,
-      onChanged: (final day) => setState(
-          () => widget._currentDate = widget._currentDate.copyWith(day: day)),
-    );
-  }
-
-  List<Widget> _buildDropdownButtonsByDateFormat() {
-    final dropdownButtonList = <Widget>[];
-
+  /// Returns date as String by a [separator]
+  /// based on [dateFormat]
+  String getDate([String separator = '-']) {
+    final date = currentDate;
+    final year = date.year;
+    final month = Date.toStringWithLeadingZeroIfLengthIsOne(date.month);
+    final day = Date.toStringWithLeadingZeroIfLengthIsOne(date.day);
     switch (widget.dateFormat) {
-      case DateFormat.dmy:
-        dropdownButtonList
-          ..add(_buildDayDropdownButton())
-          ..add(_buildMonthDropdownButton())
-          ..add(_buildYearDropdownButton());
-        break;
       case DateFormat.ymd:
-        dropdownButtonList
-          ..add(_buildYearDropdownButton())
-          ..add(_buildMonthDropdownButton())
-          ..add(_buildDayDropdownButton());
-        break;
+        return '$year$separator$month$separator$day';
+      case DateFormat.dmy:
+        return '$day$separator$month$separator$year';
       case DateFormat.mdy:
-        dropdownButtonList
-          ..add(_buildMonthDropdownButton())
-          ..add(_buildDayDropdownButton())
-          ..add(_buildYearDropdownButton());
-        break;
+        return '$month$separator$day$separator$year';
     }
-    return dropdownButtonList;
   }
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: _buildDropdownButtonsByDateFormat(),
+    final theme = Theme.of(context);
+    final maxDay = DateUtil.daysInDate(
+      month: currentDate.month,
+      year: currentDate.year,
     );
+    return Row(
+      children: [
+        _DropdownMenuButton<int?>(
+          value: currentDate.year,
+          textStyle: widget.textStyle,
+          underline: widget.underline,
+          hintText: widget.dateHint.year,
+          onChanged: (int? value) {
+            _currentDate = currentDate.copyWith(year: value);
+          },
+          items: _buildDropdownMenuItemList(
+            widget.firstYear,
+            widget.lastYear,
+            false,
+          ),
+        ),
+        Text(
+          "年",
+          style: theme.textTheme.caption,
+        ),
+        const SizedBox(width: 12.0),
+        _DropdownMenuButton<int?>(
+          value: currentDate.month,
+          textStyle: widget.textStyle,
+          underline: widget.underline,
+          hintText: widget.dateHint.month,
+          onChanged: (int? value) {
+            _currentDate = currentDate.copyWith(month: value);
+          },
+          items: _buildDropdownMenuItemList(1, 12, true),
+        ),
+        Text(
+          "月",
+          style: theme.textTheme.caption,
+        ),
+        const SizedBox(width: 12.0),
+        _DropdownMenuButton<int?>(
+          value: currentDate.day,
+          textStyle: widget.textStyle,
+          underline: widget.underline,
+          hintText: widget.dateHint.day,
+          onChanged: (int? value) {
+            _currentDate = currentDate.copyWith(day: value);
+          },
+          items: _buildDropdownMenuItemList(1, maxDay, true),
+        ),
+        Text(
+          "日",
+          style: theme.textTheme.caption,
+        ),
+      ],
+    );
+  }
+}
+
+class _DropdownMenuButton<T> extends StatelessWidget {
+  const _DropdownMenuButton({
+    Key? key,
+    this.value,
+    this.textStyle,
+    this.hintText,
+    this.underline,
+    this.onChanged,
+    this.items,
+  }) : super(key: key);
+
+  final T? value;
+
+  final TextStyle? textStyle;
+
+  final String? hintText;
+
+  final Widget? underline;
+
+  final ValueChanged<T?>? onChanged;
+
+  final List<DropdownMenuItem<T>>? items;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final hintWidget = hintText != null
+        ? Text(
+            hintText!,
+            style: TextStyle(
+              color: theme.disabledColor,
+            ),
+          )
+        : null;
+    return DropdownButton<T>(
+      style: textStyle,
+      underline: underline,
+      value: value,
+      hint: hintWidget,
+      onChanged: onChanged,
+      items: items,
+    );
+  }
+}
+
+class _Underline extends StatelessWidget {
+  const _Underline({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Container(
+      color: theme.dividerColor,
+      height: 1.0,
+    );
+  }
+}
+
+List<DropdownMenuItem<int?>> _buildDropdownMenuItemList(
+    int min, int max, bool ascending) {
+  return _intGenerator(min, max, ascending)
+      .map(
+        (i) => DropdownMenuItem<int?>(
+          value: i,
+          child: Center(
+            child: Text(
+              _toStringWithLeadingZeroIfLengthIsOne(i),
+            ),
+          ),
+        ),
+      )
+      .toList();
+}
+
+String _toStringWithLeadingZeroIfLengthIsOne(int? i) {
+  if (i == null || i < 0) {
+    return '';
+  }
+  return Date.toStringWithLeadingZeroIfLengthIsOne(i);
+}
+
+Iterable<int?> _intGenerator(int start, int end, bool ascending) sync* {
+  yield null;
+  if (ascending) {
+    for (var i = start; i <= end; i++) {
+      yield i;
+    }
+  } else {
+    for (var i = end; i >= start; i--) {
+      yield i;
+    }
   }
 }
